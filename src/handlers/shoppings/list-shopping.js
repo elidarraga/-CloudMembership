77ab35a52ac8217e318d9519c2a6a5efb815c241
elidarraga <elkindanni@hotmail.com>
2023-03-05 16:51:10 -0500

    Initial commit

---
+++ src/handlers/shoppings/list-shopping.js
@@ -0,0 +1,21 @@
+const { listData } = require('../../controllers/shoppingsController');
+const { listDataId } = require('../../controllers/shoppingsController');
+const { headers } = require('../../utils/http-utils');
+
+module.exports.handler = async (event, _context, _callback) => {
+    console.log("[DEBUG - List Shippings]", event);
+
+    // const response = await scan({ TableName: `Shoppings-${process.env.ENV}` });
+    const id = event.pathParameters?.shoppingId;
+    let response = !id ? await listData() :  await listDataId(id);
+    return {
+        statusCode: 200,
+        headers,
+        body: JSON.stringify({
+            error: false,
+            body: JSON.stringify(response)
+        })
+    }
+
+
+}
